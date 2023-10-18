2fbabf2aa29e875948b870cd0b4063456b1275e2
Abhishek Kumar Kushwaha <102471941+Abhicodeitout@users.noreply.github.com>
2023-10-18 15:57:48 +0000

    Adding handleFrm function function with html.

--- main.go
+++ main.go
@@ -17,3 +17,28 @@ func main(){
 	http.HandleFunc("/short/", HandleRdirect)
 	http.ListenAndServe("":3030", nil)
 }
+
+func handleFrm(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		http.Redirect(w, r, "/shorturl", http.StatusSeeOther)
+		return
+	}
+
+	// Serve the HTML form
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>URL Shortener</title>
+		</head>
+		<body>
+			<h2>URL Shortener</h2>
+			<form method="post" action="/shorturl">
+				<input type="url" name="url" placeholder="Enter a URL" required>
+				<input type="submit" value="Shorturl">
+			</form>
+		</body>
+		</html>
+	`)
+}
